239d0033ebc342da164defb28546c86d01efadf8
tarasiukv <tarasiuk.viktor.o.m@gmail.com>
2024-08-01 18:41:57 +0300

    New:  - Models: ExchangeRate.  - Controllers: ExchangeRate.  - Interfaces: ExchangeRate.  - Repositories: ExchangeRate.  - Resource: ExchangeRate.  - Request: ExchangeRate. Update: Delete:

---
+++ app/Http/Resources/ExchangeRateResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ExchangeRateResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'currency_id' => $this->currency_id,
+            'currency' => new CurrencyResource($this->whenLoaded('currency')),
+            'ask' => $this->ask,
+            'bid' => $this->bid,
+            'created_at' => $this->created_at ? $this->created_at->format('d.m.Y, H:i') : null,
+            'updated_at' => $this->updated_at ? $this->updated_at->format('d.m.Y, H:i') : null,
+        ];
+    }
+}
---
+++ app/Models/ExchangeRate.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ExchangeRate extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'currency_id',
+        'ask',
+        'bid',
+    ];
+
+    public function currency()
+    {
+        return $this->belongsTo(Currency::class);
+    }
+}
