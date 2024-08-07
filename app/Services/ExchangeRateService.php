ef86a52d36828eecdf4a48475e11e40871ba6e4f
tarasiukv <tarasiuk.viktor.o.m@gmail.com>
2024-08-07 11:23:32 +0300

    Updated: ExchangeRateService

--- app/Services/ExchangeRateService.php
+++ app/Services/ExchangeRateService.php
@@ -29,38 +29,18 @@ class ExchangeRateService
 
     public function getExchangeRates()
     {
-//        $api_keys = $this->getApiKey();
-//
-//        $url = "https://v6.exchangerate-api.com/v6/{$api_keys['erapi']}/latest/USD";
-//
-//        $client = new Client();
-//        $response = $client->get($url);
-//
-//        $data = json_decode($response->getBody(), true);
-//
-//        if (!isset($data['conversion_rates'])) {
-//            return response()->json(['error' => 'Missing conversion rates'], 500);
-//        }
+        $api_keys = $this->getApiKey();
 
-        $data = [
-            "result" => "success",
-            "documentation" => "https://www.exchangerate-api.com/docs",
-            "terms_of_use" => "https://www.exchangerate-api.com/terms",
-            "time_last_update_unix" => 1722902401,
-            "time_last_update_utc" => "Tue, 06 Aug 2024 00:00:01 +0000",
-            "time_next_update_unix" => 1722988801,
-            "time_next_update_utc" => "Wed, 07 Aug 2024 00:00:01 +0000",
-            "base_code" => "USD",
-            "conversion_rates" => [
-                "USD" => 1,
-                "AED" => 3.6725,
-                "AFN" => 70.6389,
-                "ALL" => 91.5840,
-                "AMD" => 387.9398,
-                "UAH" => 41.0398,
-                "ZWL" => 3.7654
-            ]
-        ];
+        $url = "https://v6.exchangerate-api.com/v6/{$api_keys['erapi']}/latest/USD";
+
+        $client = new Client();
+        $response = $client->get($url);
+
+        $data = json_decode($response->getBody(), true);
+
+        if (!isset($data['conversion_rates'])) {
+            return response()->json(['error' => 'Missing conversion rates'], 500);
+        }
 
         foreach ($data['conversion_rates'] as $currency_code => $rate_data) {
             $from_currency = $this->findByCode($currency_code);
