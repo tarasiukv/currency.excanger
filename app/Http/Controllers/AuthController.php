62a22e9faff4cf760df10cd4f433898247382618
tarasiukv <tarasiuk.viktor.o.m@gmail.com>
2024-08-13 11:36:44 +0300

    Update: AuthController

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\UserRegisterRequest;
+use Illuminate\Http\Client\ConnectionException;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
 
 class AuthController extends Controller
@@ -17,31 +19,53 @@ class AuthController extends Controller
      * @param UserRegisterRequest $request
      * @return \Illuminate\Http\JsonResponse
      */
-    public function register(UserRegisterRequest $request)
+    public function register(UserRegisterRequest $request): \Illuminate\Http\JsonResponse
     {
+//        $user = User::create([
+//                'name' => $request->name,
+//                'email' => $request->email,
+//                'password' => bcrypt($request->password),
+//            ]);
+
         try {
-            DB::beginTransaction();
-
-            $user = User::create([
-                'name' => $request->name,
-                'email' => $request->email,
-                'password' => bcrypt($request->password),
+            $response = Http::asForm()->post('http://127.0.0.1:8000/oauth/token', [
+                'grant_type' => 'password',
+                'client_id' => config('passport.grant_password_client.id'),
+                'client_secret' => config('passport.grant_password_client.secret'),
+                'username' => $request->email,
+                'password' => $request->password,
+                'scope' => '*',
             ]);
-
-            $token = $user->createToken('Personal Access Token')->accessToken;
-
-            Log::channel('auth')->info("Register info: User id:{$user->id} created");
-            DB::commit();
-
-            return response()->json(['token' => $token], 200);
-
-        } catch (\Exception $e) {
-            Log::channel('auth')->error("Register error: {$e}");
-            DB::rollBack();
-
-            return response()->json(['error' => 'User not registered'], 500);
-
+        } catch (ConnectionException $e) {
+            dd('ERROR', $e);
         }
+
+        return $response->json();
+
+
+//        try {
+//            DB::beginTransaction();
+//
+//            $user = User::create([
+//                'name' => $request->name,
+//                'email' => $request->email,
+//                'password' => bcrypt($request->password),
+//            ]);
+//
+//            $token = $user->createToken('Personal Access Token')->accessToken;
+//
+//            Log::channel('auth')->info("Register info: User id:{$user->id} created");
+//            DB::commit();
+//
+//            return response()->json(['token' => $token], 200);
+//
+//        } catch (\Exception $e) {
+//            Log::channel('auth')->error("Register error: {$e}");
+//            DB::rollBack();
+//
+//            return response()->json(['error' => 'User not registered'], 500);
+//
+//        }
     }
 
     /**
@@ -68,9 +92,13 @@ class AuthController extends Controller
 
             DB::commit();
 
-            return response()->json(['message' => 'Profile updated successfully'], 200);
+            return response()->json(['message' => 'User updated successfully'], 200);
 
         } catch (\Exception $e) {
+            Log::channel('auth')->error("Update error: {$e}");
+            DB::rollBack();
+
+            return response()->json(['error' => 'User not updated'], 500);
 
         }
     }
@@ -81,7 +109,7 @@ class AuthController extends Controller
      * @param Request $request
      * @return \Illuminate\Http\JsonResponse
      */
-    public function login(Request $request)
+    public function login(Request $request): \Illuminate\Http\JsonResponse
     {
         $credentials = [
             'email' => $request->email,
@@ -107,5 +135,18 @@ class AuthController extends Controller
             return response()->json(['error' => 'An error occurred while trying to log in'], 500);
 
         }
+    }
+
+    /**
+     * Logout user
+     *
+     */
+    public function logout()
+    {
+        $user = Auth::user();
+        dd(1111111111);
+        Log::channel('auth')->info("Logout info: User id:{$user->id} logged out");
+
+        return response()->json($user->token()->revoke(), 200);
     }
 }
