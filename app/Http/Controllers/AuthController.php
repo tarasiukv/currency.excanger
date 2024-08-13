c33ef4ab03d59a9d53679f94c3b32a35ea706317
tarasiukv <tarasiuk.viktor.o.m@gmail.com>
2024-08-14 01:06:43 +0300

    Update: AuthController, api.php

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,48 +2,64 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\PasswordRequest;
 use App\Http\Requests\UserRegisterRequest;
-use Illuminate\Http\Client\ConnectionException;
-use Illuminate\Http\Request;
 use App\Models\User;
+use App\Traits\HandlesOAuthRequests;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
 
 class AuthController extends Controller
 {
+    use HandlesOAuthRequests;
+
     /**
      * Register user
      *
      * @param UserRegisterRequest $request
-     * @return \Illuminate\Http\JsonResponse
      */
-    public function register(UserRegisterRequest $request): \Illuminate\Http\JsonResponse
+    public function register(UserRegisterRequest $request)
     {
-//        $user = User::create([
-//                'name' => $request->name,
-//                'email' => $request->email,
-//                'password' => bcrypt($request->password),
-//            ]);
+        DB::beginTransaction();
 
         try {
-            $response = Http::asForm()->post('http://127.0.0.1:8000/oauth/token', [
-                'grant_type' => 'password',
-                'client_id' => config('passport.grant_password_client.id'),
-                'client_secret' => config('passport.grant_password_client.secret'),
-                'username' => $request->email,
-                'password' => $request->password,
-                'scope' => '*',
+            $user = User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => bcrypt($request->password),
             ]);
-        } catch (ConnectionException $e) {
-            dd('ERROR', $e);
+
+            $response = $this->sendOAuthRequest('password', [
+                'username' => $user->email,
+                'password' => $request->password,
+            ]);
+
+            if (!$response->successful()) {
+                Log::channel('auth')->error("Register error: {$response->json()}");
+                DB::rollBack();
+
+                return response()->json(['error' => 'User registration failed.'], 500);
+            }
+
+            Log::channel('auth')->info("Register info: User id:{$user->id} created.");
+            DB::commit();
+
+            return response()->json([
+                'user' => $user,
+                'token' => $response->json()
+            ], 201);
+
+        } catch (\Exception $e) {
+
+            Log::channel('auth')->error("Register error: {$e->getMessage()}");
+            DB::rollBack();
+
+            return response()->json(['error' => 'An error occurred during registration.'], 500);
         }
 
-        return $response->json();
-
-
-//        try {
+        //try {
 //            DB::beginTransaction();
 //
 //            $user = User::create([
@@ -69,41 +85,6 @@ class AuthController extends Controller
     }
 
     /**
-     * Update user
-     *
-     * @param UserRegisterRequest $request
-     * @return \Illuminate\Http\JsonResponse|void
-     */
-    public function updateProfile(UserRegisterRequest $request)
-    {
-        // TODO: in progress
-        try {
-            DB::beginTransaction();
-
-            $user = Auth::user();
-            $user->name = $request->name;
-            $user->email = $request->email;
-
-            if ($request->password) {
-                $user->password = bcrypt($request->password);
-            }
-
-            $user->save();
-
-            DB::commit();
-
-            return response()->json(['message' => 'User updated successfully'], 200);
-
-        } catch (\Exception $e) {
-            Log::channel('auth')->error("Update error: {$e}");
-            DB::rollBack();
-
-            return response()->json(['error' => 'User not updated'], 500);
-
-        }
-    }
-
-    /**
      * Login user
      *
      * @param Request $request
@@ -119,34 +100,80 @@ class AuthController extends Controller
         try {
             if (Auth::attempt($credentials)) {
                 $user = Auth::user();
-                $token = $user->createToken('Personal Access Token')->accessToken;
+
+                $response = $this->sendOAuthRequest('password', [
+                    'username' => $user->email,
+                    'password' => $request->password,
+                ]);
 
                 Log::channel('auth')->info("Login info: User id:{$user->id} logged in");
 
-                return response()->json(['token' => $token], 200);
-
+                return $response->json();
             } else {
                 Log::channel('auth')->warning("Login warning: Failed login attempt for email:{$request->email}");
-                return response()->json(['error' => 'Unauthorized'], 401);
 
+                return response()->json(['error' => 'Unauthorized'], 401);
             }
         } catch (\Exception $e) {
             Log::channel('auth')->error("Login error: {$e}");
-            return response()->json(['error' => 'An error occurred while trying to log in'], 500);
 
+            return response()->json(['error' => 'An error occurred while trying to log in'], 500);
         }
+    }
+
+    /**
+     * User personal info
+     *
+     */
+    public function personalInfo()
+    {
+        try {
+            $user = Auth::user();
+
+            Log::channel('auth')->info("PersonalInfo info: User id:{$user->id}");
+
+            return $user;
+        } catch (\Exception $e) {
+            Log::channel('auth')->error("PersonalInfo error: {$e}");
+
+            return response()->json(['error' => 'An error occurred while trying to PersonalInfo'], 500);
+        }
+    }
+
+    public function changePassword(PasswordRequest $request)
+    {
+        dd($request);
+        $user = Auth::user();
+        $user->update($request->all());
+        return $user->refresh();
     }
 
     /**
      * Logout user
      *
+     * @return \Illuminate\Http\JsonResponse
      */
-    public function logout()
+    public function logout(): \Illuminate\Http\JsonResponse
     {
-        $user = Auth::user();
-        dd(1111111111);
-        Log::channel('auth')->info("Logout info: User id:{$user->id} logged out");
+        try {
+            $user = Auth::user();
 
-        return response()->json($user->token()->revoke(), 200);
+            if ($user && $user->token()) {
+
+                $user->token()->revoke();
+
+                Log::channel('auth')->info("Logout info: User id:{$user->id} logged out");
+
+                return response()->json(['message' => 'Successfully logged out'], 200);
+            } else {
+                Log::channel('auth')->warning("Logout warning: Attempt to logout without valid token");
+
+                return response()->json(['error' => 'No valid token found'], 400);
+            }
+        } catch (\Exception $e) {
+            Log::channel('auth')->error("Logout error: {$e}");
+
+            return response()->json(['error' => 'An error occurred while trying to log out'], 500);
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -18,15 +18,15 @@ Route::group(['prefix' => 'auth'], function () {
     Route::post('register', [AuthController::class, 'register']);
     Route::post('login', [AuthController::class, 'login']);
 
-//    Route::group(['middleware' => 'auth:api'], function () {
-//        Route::post('logout', [AuthController::class, 'logout']);
-//        Route::post('refresh', [AuthController::class, 'logout']);
-//    });
     Route::middleware('auth:api')->group(function () {
+        Route::patch('password', [AuthController::class, 'changePassword']);
         Route::post('logout', [AuthController::class, 'logout']);
-        Route::post('refresh', [AuthController::class, 'logout']);
+        Route::post('refresh', [AuthController::class, 'refresh']);
     });
+});
 
+Route::middleware('auth:api')->group(function () {
+    Route::get('personal-info', [AuthController::class, 'personalInfo']);
 });
 
 //Route::middleware('auth')->group(function () {
