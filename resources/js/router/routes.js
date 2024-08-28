1c5d0e104364272aea0979c747c895e1a4b2c85e
tarasiukv <tarasiuk.viktor.o.m@gmail.com>
2024-08-28 13:11:32 +0300

    New: route, vite bootstrap, app, App

--- resources/js/router/routes.js
+++ resources/js/router/routes.js
@@ -1,4 +1,6 @@
-import HomeComponent from "@component/HomeComponent.vue";
+import HomeComponent from "@components/HomeComponent.vue";
+import LoginComponent from "@components/auth/LoginComponent.vue";
+import RegisterComponent from "@components/auth/RegisterComponent.vue";
 
 const routes = [
     {
@@ -6,6 +8,24 @@ const routes = [
         component: HomeComponent,
         meta: {title: 'Home | CurEx'}
     },
+    {
+        path: '/login',
+        component: LoginComponent,
+        meta: {title: 'Login | CurEx'}
+    },
+    {
+        path: '/register',
+        component: RegisterComponent,
+        meta: {title: 'Register | CurEx'}
+    },
+    // {
+    //     path: '/dashboard',
+    //     component: DashboardComponent,
+    //     meta: {
+    //     title: 'Dashboard | CurEx',
+    //         requiresAuth: true,
+    //     }
+    // },
 ];
 
 export default routes;
